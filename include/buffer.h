c5dc3e7f694f8d15c2898f09f7f5bf220caf67e4
szeyoong-low <low.szeyoong@gmail.com>
2026-06-23 11:41:43 +0100

    style: renamed parameters of buffer initialisers to reflect flexibility of sizes

--- include/buffer.h
+++ include/buffer.h
@@ -27,12 +27,11 @@ typedef struct {
 extern void attribute_buffer_init(GLuint *attribute_buffer);
 extern void vertex_buffer_init(GLuint *vertex_buffer);
 extern void element_buffer_init(GLuint *element_buffer);
-extern void render_info_buffer_init(GLuint *render_info_buffer, size_t sim_size,
+extern void render_info_buffer_init(GLuint *render_info_buffer, size_t size,
                                     const RenderInfo *data);
-extern void hidden_cell_buffer_init(GLuint *hidden_cell_buffer,
-                                    size_t sim_size);
-extern void instance_buffer_init(GLuint *instance_buffer, size_t sim_size);
+extern void hidden_cell_buffer_init(GLuint *hidden_cell_buffer, size_t size);
+extern void instance_buffer_init(GLuint *instance_buffer, size_t size);
 extern void draw_indirect_buffer_init(GLuint *draw_indirect_buffer);
-extern void sort_key_buffer_init(GLuint *sort_key_buffer, size_t sim_size);
+extern void sort_key_buffer_init(GLuint *sort_key_buffer, size_t size);
 
 #endif
