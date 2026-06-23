a59196f3b85b25e0b1bf5f34ed3f0154fa4fc23a
szeyoong-low <low.szeyoong@gmail.com>
2026-06-23 12:49:27 +0100

    refactor: changed logic of hidden cell checks to be per-face

--- shaders/hidden.glsl
+++ shaders/hidden.glsl
@@ -24,8 +24,27 @@ uniform uint uWidth;
 uniform uint uHeight;
 uniform uint uDepth;
 
-const uint opacityFloor = 20;
-const uint opacityCeiling = 200;
+const uint opacityFloor = 20; // Below which, a cell is considered transparent
+const uint opacityCeiling = 200; // Above which, a cell is considered opaque
+
+// Precondition: the cube at selfIndex is not transparent and is not at the
+//               boundary of the simulation grid
+bool check_hidden(uint selfIndex, uint otherIndex) {
+  const uint selfAlpha = ALPHA(selfIndex);
+  const uint otherAlpha = ALPHA(otherIndex);
+
+  // 1. Faces at the surface of a cluster of cubes should be rendered
+  //    unconditionally
+  return (otherAlpha >= opacityFloor) &&
+  // 2. A boundary between two cubes of the same packed colour should be
+  //    culled unconditionally
+         (renderInfo[selfIndex] == renderInfo[otherIndex] ||
+  // 3. A boundary between two cubes of different packed colours should
+  //    be rendered if and only if the other cube is at or below the
+  //    opacity ceiling. If both cells are below the opacity ceiling,
+  //    the back face will be culled later on.
+          otherAlpha > opacityCeiling);
+}
 
 void main() {
   const uint x = gl_GlobalInvocationID.x;
@@ -38,24 +57,29 @@ void main() {
   }
 
   const uint selfIndex = INDEX(x, y, z, uWidth, uHeight);
-  const uint selfColour = ALPHA(selfIndex);
   uint hidden = FULL_CUBE;
 
-  if (selfColour < opacityFloor) {
-    // Cells below the opacity floor are considered transparent, so all faces
-    // are hidden (the whole cell is culled)
+  if (ALPHA(selfIndex) < opacityFloor) {
+    // All faces of transparent cells are hidden (the whole cell is culled)
     hidden = HIDDEN_CUBE;
   } else if (x == 0 || x == uWidth - 1 || y == 0 || y == uHeight - 1 ||
              z == 0 || z == uDepth - 1) {
     // Visible cells at the edge of the simulation grid are never culled
     hidden = FULL_CUBE;
   } else {
-    hidden |= uint(ALPHA(INDEX(x, y, z + 1, uWidth, uHeight)) > opacityCeiling) << FRONT_FACE_OFFSET;
-    hidden |= uint(ALPHA(INDEX(x, y, z - 1, uWidth, uHeight)) > opacityCeiling) << BACK_FACE_OFFSET;
-    hidden |= uint(ALPHA(INDEX(x - 1, y, z, uWidth, uHeight)) > opacityCeiling) << LEFT_FACE_OFFSET;
-    hidden |= uint(ALPHA(INDEX(x + 1, y, z, uWidth, uHeight)) > opacityCeiling) << RIGHT_FACE_OFFSET;
-    hidden |= uint(ALPHA(INDEX(x, y - 1, z, uWidth, uHeight)) > opacityCeiling) << BOTTOM_FACE_OFFSET;
-    hidden |= uint(ALPHA(INDEX(x, y + 1, z, uWidth, uHeight)) > opacityCeiling) << TOP_FACE_OFFSET;
+    // Check faces individually
+    hidden |= uint(check_hidden(selfIndex, INDEX(x, y, z + 1, uWidth, uHeight)))
+              << FRONT_FACE_OFFSET;
+    hidden |= uint(check_hidden(selfIndex, INDEX(x, y, z - 1, uWidth, uHeight)))
+              << BACK_FACE_OFFSET;
+    hidden |= uint(check_hidden(selfIndex, INDEX(x - 1, y, z, uWidth, uHeight)))
+              << LEFT_FACE_OFFSET;
+    hidden |= uint(check_hidden(selfIndex, INDEX(x + 1, y, z, uWidth, uHeight)))
+              << RIGHT_FACE_OFFSET;
+    hidden |= uint(check_hidden(selfIndex, INDEX(x, y - 1, z, uWidth, uHeight)))
+              << BOTTOM_FACE_OFFSET;
+    hidden |= uint(check_hidden(selfIndex, INDEX(x, y + 1, z, uWidth, uHeight)))
+              << TOP_FACE_OFFSET;
   }
 
   hiddenCells[selfIndex] = hidden;
