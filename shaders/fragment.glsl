6f2f57eee6776e25daf9147280ec7201bbbbc1bf
szeyoong-low <low.szeyoong@gmail.com>
2026-06-24 22:48:15 +0100

    feat: switch from Phong to Blinn-Phong lighting model

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -18,7 +18,7 @@ out vec4 fragColor;
 //             and observer (light enters your eye -> see bright spots)
 float ambientStrength = 0.7F;
 float specularStrength = 0.7F;
-float shininess = 64.0F;
+float shininess = 128.0F;
 vec3 lightColor = vec3(1.0F, 1.0F, 1.0F); // white light
 // Calculated by vertex shaders
 in vec3 vNormal;
@@ -35,12 +35,12 @@ void main() {
     vec3 norm = normalize(vNormal);
     vec3 lightDir = normalize(uLightPos - vFragPos);
     vec3 cameraDir = normalize(uCameraPos - vFragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
+    vec3 halfwayDir = normalize(lightDir + cameraDir);
 
     vec3 ambient = ambientStrength * lightColor;
     vec3 diffuse = max(dot(norm, lightDir), 0.0F) * lightColor;
     vec3 specular = specularStrength *
-                    pow(max(dot(cameraDir, reflectDir), 0.0F), shininess) *
+                    pow(max(dot(norm, halfwayDir), 0.0F), shininess) *
                     lightColor;
 
     // Component-wise multiplication
