10ef38bf421d698f3c59da515bb56a0fda80d98c
szeyoong-low <low.szeyoong@gmail.com>
2026-06-26 00:13:08 +0100

    feat: re-implemented shaders

--- shaders/post_process_fragment.glsl
+++ shaders/post_process_fragment.glsl
@@ -2,14 +2,25 @@
 
 // Highest resolution (though there's only one)
 #define HIGHEST_MIP 0
+#define MIN_ACCUM_ALPHA 1e-5F
+#define REVEAL_COMPLEMENT 1.0F
+
+#define ACCUM_COLOR_ATTACHMENT 0
+#define REVEAL_COLOR_ATTACHMENT 1
 
 // Input texture to sample
-uniform sampler2D uTexture;
+// bind your accum render target to this texture unit
+layout(binding = ACCUM_COLOR_ATTACHMENT) uniform sampler2D uAccumTexture;
+
+// bind your reveal render target to this texture unit
+layout(binding = REVEAL_COLOR_ATTACHMENT) uniform sampler2D uRevealTexture;
 
 // out: goes to framebuffer
 out vec4 fragColor;
 
 void main() {
-  // Simply copy the texture into the framebuffer
-  fragColor = texelFetch(uTexture, ivec2(gl_FragCoord.xy), HIGHEST_MIP);
+  // Source: https://learnopengl.com/Guest-Articles/2020/OIT/Weighted-Blended
+  const vec4 accum = texelFetch(uAccumTexture, ivec2(gl_FragCoord.xy), HIGHEST_MIP);
+  const float reveal = texelFetch(uRevealTexture, ivec2(gl_FragCoord.xy), HIGHEST_MIP).r;
+  fragColor = vec4(accum.rgb / max(accum.a, MIN_ACCUM_ALPHA), (REVEAL_COMPLEMENT - reveal));
 }
